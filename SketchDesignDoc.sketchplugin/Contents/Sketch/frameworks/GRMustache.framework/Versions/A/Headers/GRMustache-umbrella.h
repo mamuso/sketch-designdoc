de9743e7420a728448fe7e7fa5e082d7c8f71dce
Manuel Muñoz Solera <mamuso@mamuso.net>
2016-02-15 19:27:20 -0800

    Adding GRMustache to the party

---
+++ SketchDesignDoc.sketchplugin/Contents/Sketch/frameworks/GRMustache.framework/Versions/A/Headers/GRMustache-umbrella.h
@@ -0,0 +1,23 @@
+#import <Cocoa/Cocoa.h>
+
+#import "GRMustacheTag.h"
+#import "GRMustacheConfiguration.h"
+#import "GRMustache.h"
+#import "GRMustacheVersion.h"
+#import "GRMustacheContext.h"
+#import "GRMustacheFilter.h"
+#import "GRMustacheRendering.h"
+#import "GRMustacheSafeKeyAccess.h"
+#import "GRMustacheTagDelegate.h"
+#import "NSFormatter+GRMustache.h"
+#import "NSValueTransformer+GRMustache.h"
+#import "GRMustacheLocalizer.h"
+#import "GRMustacheAvailabilityMacros.h"
+#import "GRMustacheContentType.h"
+#import "GRMustacheError.h"
+#import "GRMustacheTemplate.h"
+#import "GRMustacheTemplateRepository.h"
+
+FOUNDATION_EXPORT double GRMustacheVersionNumber;
+FOUNDATION_EXPORT const unsigned char GRMustacheVersionString[];
+
